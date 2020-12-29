cd5aa3717ad8b65db0a92da53599b5e0752f1336
Filipe Leuch Bonfim <filipe1309@gmail.com>
2020-12-28 22:45:51 -0300

    refactor: replace fopen with file_get_contents

--- leitor-cursos.php
+++ leitor-cursos.php
@@ -1,14 +1,5 @@
 <?php
 
-$arquivo = fopen('lista-cursos.txt', 'r');
-
-// Ok para arquivos pequenos
-// Perigo de alto consumo de memória,
-// pois se o arquivo for muito grande irá colocar todo em memória 
-// sendo que o limite padrão do php é de 128MB
-$tamanhoDoArquivoBytes = filesize('lista-cursos.txt');
-$cursos = fread($arquivo, $tamanhoDoArquivoBytes);
+$cursos = file_get_contents('lista-cursos.txt');
 
 echo $cursos;
-
-fclose($arquivo);
