ee832956e742e7b356bff6705cf11b6639e7e1d3
Filipe Leuch Bonfim <filipe1309@gmail.com>
2020-12-28 22:41:24 -0300

    refactor: change read file method

--- leitor-cursos.php
+++ leitor-cursos.php
@@ -2,9 +2,13 @@
 
 $arquivo = fopen('lista-cursos.txt', 'r');
 
-while (!feof($arquivo)) {
-    $curso = fgets($arquivo);
-    echo $curso;
-}
+// Ok para arquivos pequenos
+// Perigo de alto consumo de memória,
+// pois se o arquivo for muito grande irá colocar todo em memória 
+// sendo que o limite padrão do php é de 128MB
+$tamanhoDoArquivoBytes = filesize('lista-cursos.txt');
+$cursos = fread($arquivo, $tamanhoDoArquivoBytes);
+
+echo $cursos;
 
 fclose($arquivo);
