fc2e05a64c6940e84928dd2729e3aec3d26810af
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-01-05 00:21:02 -0300

    chore: change file creation from fopen to file_put_contents

--- escritor-cursos.php
+++ escritor-cursos.php
@@ -1,9 +1,7 @@
 <?php
 
-$arquivo = fopen('cursos-php.txt', 'a');
+$curso1 = 'Design Patterns em PHP: Padrões comportamentais';
+file_put_contents('cursos-php.txt', $curso1);
 
-$curso = "\nDesign Patterns em PHP: Padrões estruturais";
-
-fwrite($arquivo, $curso);
-
-fclose($arquivo);
+$curso2 = "\nDesign Patterns em PHP: Padrões estruturais";
+file_put_contents('cursos-php.txt', $curso2, FILE_APPEND);
