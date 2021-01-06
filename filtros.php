96149f444ccfd2a599d5d9c1921e1cd8433aa243
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-01-06 00:10:41 -0300

    feat: add custom stream filter

--- filtros.php
+++ filtros.php
@@ -1,6 +1,12 @@
 <?php
 
+require 'MeuFiltro.php';
+
 $arquivosCursos = fopen('lista-cursos.txt', 'r');
+
+// Custom stream filter
+stream_filter_register('alura.patterns', MeuFiltro::class);
+stream_filter_append($arquivosCursos, 'alura.patterns');
 
 stream_filter_append($arquivosCursos, 'string.toupper');
 
