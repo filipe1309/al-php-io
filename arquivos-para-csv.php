6da6f2852aa36fd5b782ae95f46cad7df0d75072
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-01-08 00:03:02 -0300

    feat: insert into a csv file with fputcsv

---
+++ arquivos-para-csv.php
@@ -0,0 +1,31 @@
+<?php
+
+$meusCursos = file('lista-cursos.txt');
+$outrosCursos = file('cursos-php.txt');
+
+$arquivoCsv = fopen('cursos.csv', 'w');
+
+foreach ($meusCursos as $curso) {
+    $linha = [ trim($curso), 'Sim' ];
+    fputcsv($arquivoCsv, $linha, ';');
+    // fwrite($arquivoCsv, implode(';', $linha));
+}
+
+foreach ($outrosCursos as $curso) {
+    $linha = [ trim($curso), 'Nao' ];
+    fputcsv($arquivoCsv, $linha, ';');
+    // fwrite($arquivoCsv, implode(';', $linha));
+}
+
+fclose($arquivoCsv);
+
+
+echo "\nResultado no CSV:\n";
+
+$arquivoCsv = fopen('cursos.csv', 'r');
+
+while (($curso = fgetcsv($arquivoCsv)) !== false) {
+    var_dump($curso);
+}
+
+fclose($arquivoCsv);
