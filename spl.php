2f2ac3a5b9632d7962bdd487c23e96d2dead254b
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-01-08 00:32:24 -0300

    refactor: add others spl examples

--- spl.php
+++ spl.php
@@ -1,5 +1,7 @@
 <?php
 
+echo 'SplFileObject read csv:' . PHP_EOL;
+
 $arquivosCursos = new SplFileObject('cursos.csv');
 
 while(!$arquivosCursos->eof()) {
@@ -7,6 +9,17 @@ while(!$arquivosCursos->eof()) {
     echo $linha[0] . PHP_EOL;
 }
 
+echo 'SplFileObject get ewxtra info from file:' . PHP_EOL;
+
 $date = new DateTime();
 $date->setTimestamp($arquivosCursos->getCTime());
-echo $date->format('d/m/Y');
+echo $date->format('d/m/Y') . PHP_EOL;
+
+
+echo PHP_EOL . 'SplFileObject with iterator:' . PHP_EOL;
+
+$arquivosCursos = new SplFileObject('cursos-php.txt');
+
+foreach($arquivosCursos as $curso) {
+    echo $curso;
+}
